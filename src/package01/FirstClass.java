006f943848fbec06fc114cb36da8a27442dd78d3
GulArmagan <g.armagan.kurtoglu@gmail.com>
2021-02-28 09:06:56 +0300

    feature brach ten ilk kommit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -6,5 +6,7 @@ public static void main(String[]args) {
 	
 	System.out.println("Ilk commit from local repository");
 	System.out.println("Ikinci commit from local repository");
+
+	System.out.println("Yeni branch olusturdum");
 }
 }
